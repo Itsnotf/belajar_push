f5d3c7acb3925b9002a546821c11e017e4cdefd2
itsnotf <faizaflah009@gmail.com>
2025-05-29 15:57:15 +0700

    feature: hero-section

--- src/components/hero-section.jsx
+++ src/components/hero-section.jsx
@@ -4,7 +4,19 @@ import { ChevronRight } from 'lucide-react'
 export default function HeroSection() {
     return (
         <section id="hero" className="pt-32 pb-20 md:pt-40 md:pb-28 bg-gradient-to-br from-purple-50 to-indigo-50">
-          
+            <div className='container mx-0 min-h-screen space-y-10 flex flex-col md:flex-row md:mx-10  w-full'>
+                <div className='w-full md:w-1/2 flex flex-col justify-center items-center text-center md:items-start md:text-start mt-10 gap-y-4'>
+                    <h1 className='text-4xl font-mono font-bold'>Belajar React</h1>
+                    <p className='text-sm font-mono'>Belajar React Disintak Itu Seru banget!</p>
+                    <div className='flex flex-row gap-5'>
+                        <button className='border px-6 rounded-md'>Contact</button>
+                        <button className='border px-6 w-fit rounded-md'>Belajar Sekarang!</button>
+                    </div>
+                </div>
+                <div className='w-full md:1/2 flex justify-center items-center '>
+                    <img src="/vite.svg" alt="" className='w-1/2 md:h-1/2' />
+                </div>
+            </div>
         </section>
     )
 }
