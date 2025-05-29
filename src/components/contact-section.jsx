450862d110de6e6b04478c60f4195f5f7dbec679
Cintanaz <cintanabilazakyyah@gmail.com>
2025-05-29 17:26:14 +0700

    update contact

--- src/components/contact-section.jsx
+++ src/components/contact-section.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 export default function ContactSection() {
     return (
         <section id="contact" className="py-20 bg-purple-50">
-         
+         <div className='text-amber-700'>contact</div>
+         <img src="/vite.svg" alt="" />
         </section>
     )
 }
