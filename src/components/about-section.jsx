41a67c3fc4ba8f90c4f52256918a66690185f990
nebulaa01 <abillakmal019@gmail.com>
2025-05-29 17:21:53 +0700

    new about

--- src/components/about-section.jsx
+++ src/components/about-section.jsx
@@ -3,7 +3,15 @@ import React from "react";
 export default function AboutSection() {
   return (
     <section id="about" className="bg-white py-16">
-    
+    <div className="w-full min-h-screen flex items-center justify-center">
+      <div className="flex flex-col items-center justify-center text-center">
+        <ul className="text-2xl font-bold text-purple-800">
+          <li>SINTAK</li>
+          <li>Pelatihan Web</li>
+          <li>AZRIN BOT</li>
+        </ul>
+      </div>
+    </div>
     </section>
   );
 }
