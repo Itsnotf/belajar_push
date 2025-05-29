06f0e540406662161c5c633d1979c344cd25923d
RinnnBee <azrinm2007@gmail.com>
2025-05-29 17:25:10 +0700

    update date

--- src/components/testimonials-section.jsx
+++ src/components/testimonials-section.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function TestimonialsSection() {
     return (
         <section id="testimonials" className="py-20 bg-white">
-    
+            <div className='text-red-900'>anjay</div>
         </section>
     )
 }
