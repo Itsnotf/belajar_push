4f91d7bfa6a2a917a3a47201e5623dfa243bbf6b
hdqsta <hattasiddiqadit@gmail.com>
2025-05-29 17:28:08 +0700

    testt

--- src/components/benefits-section.jsx
+++ src/components/benefits-section.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
-import { Award, Code, Laptop, Mail, Users } from 'lucide-react'
-
+import React from "react";
+import { Award, Code, Laptop, Mail, Users } from "lucide-react";
 
 export default function BenefitsSection() {
-    return (
-        <section id="benefits" className="py-20 bg-gray-50">
-           
-        </section>
-    )
+  return (
+    <section id="benefits" className="py-20 bg-gray-50">
+      <div className="px-5 mx-auto max-w-7xl bg-red-500">
+        <h1 className="text-white">ini benefits</h1>
+        <p>
+          benefits benefits benefits benefits benefits benefits benefits
+          benefits benefits benefits benefits benefits benefits benefits
+        </p>
+      </div>
+    </section>
+  );
 }
